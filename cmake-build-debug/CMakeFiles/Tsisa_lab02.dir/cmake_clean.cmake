7957fa2051d533d405bf7981fa1c5dca6f9c5919
Grisha777 <61342782+Grisha777@users.noreply.github.com>
2020-10-19 03:47:55 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Tsisa_lab02.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tsisa_lab02.dir/TextTable.cpp.obj"
+  "CMakeFiles/Tsisa_lab02.dir/main.cpp.obj"
+  "Tsisa_lab02.exe"
+  "Tsisa_lab02.exe.manifest"
+  "Tsisa_lab02.pdb"
+  "libTsisa_lab02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tsisa_lab02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
